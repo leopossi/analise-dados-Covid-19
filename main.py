43f9abd8feab2c5e2c26b96dc411186297a0906e
Leonardo Possi <leo@MacBook-Air-de-Leonardo.local>
2023-06-09 18:35:27 -0300

    correção de link

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import pandas as pd
 import io
 import requests
 
-url="https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SGL/2022/uf=AC/lote=1/part-00000-2c6b95ee-a526-44ae-b48f-a3975bbae6fb.c000.csv"
+url="https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SGL/2022/uf=AC/lote=1/part-00000-d5288981-2f4b-47dc-9406-6c9854cfe0d2.c000.csv"
 s=requests.get(url).content
 c=pd.read_csv(io.StringIO(s.decode('utf-8')), sep=';')
 
 print(c)
 
 print(c.dtypes)
-
-print(s.decode('utf-8'))
-
-#eu vai apadar tudo - Tatá#
