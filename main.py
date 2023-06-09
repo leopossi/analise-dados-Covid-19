e66a1b72223322aea589c7bd22a49f92f14a8abd
Leonardo Possi <leo@MacBook-Air-de-Leonardo.local>
2023-06-09 11:55:09 -0300

    acesso dos dados csv por link

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello")
+import pandas as pd
+import io
+import requests
+
+url="https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SGL/2022/uf=AC/lote=1/part-00000-2c6b95ee-a526-44ae-b48f-a3975bbae6fb.c000.csv"
+s=requests.get(url).content
+c=pd.read_csv(io.StringIO(s.decode('utf-8')), sep=';')
+
+print(c)
+
+print(c.dtypes)
+
+print(s.decode('utf-8'))
